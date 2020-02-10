14025e433622248b185b5c9810ecbd673b74229b
Adarsh Mamidpelliwar <adarshmamidpelliwar@gmail.com>
2020-02-10 10:33:42 +0530

    Add files via upload

---
+++ excelr converting json into dataframe.py
@@ -0,0 +1,29 @@
+import json
+import gzip
+
+def parse(path):
+  g = gzip.open(path, 'r')
+  for l in g:
+    yield json.dumps(eval(l))
+
+f = open("output.strict", 'w')
+for l in parse("C:/Users/mohammed tanveer/Downloads/qa_Electronics.json.gz"):
+  f.write(l + '\n')
+
+import pandas as pd
+import gzip
+
+def parse(path):
+  g = gzip.open(path, 'rb')
+  for l in g:
+    yield eval(l)
+
+def getDF(path):
+  i = 0
+  df = {}
+  for d in parse(path):
+    df[i] = d
+    i += 1
+  return pd.DataFrame.from_dict(df, orient='index')
+
+df = getDF('C:/Users/mohammed tanveer/Downloads/qa_Electronics.json.gz')
